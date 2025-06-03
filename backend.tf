fcb38eefd57225bcf053c41238383831a48b7b55
sai-kumar9020 <100404258+sai-kumar9020@users.noreply.github.com>
2025-06-03 21:07:52 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,10 +2,8 @@ terraform {
   required_version = ">= 1.10.0"
 
   backend "s3" {
-    bucket       = "techopsbucket123"
+    bucket       = "hcltrainings"
     key          = "vpcec2/terraform.tfstatefocalboard"
     region       = "us-east-1"
-    encrypt      = true
-    use_lockfile = true
   }
 }
