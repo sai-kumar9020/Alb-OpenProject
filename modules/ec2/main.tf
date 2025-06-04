94587a3c0533c76de3382f59f99c9b84b3ca3558
sai-kumar9020 <100404258+sai-kumar9020@users.noreply.github.com>
2025-06-04 09:40:23 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web-servers" {
-  ami                    = var.ami_name
+  ami                    = var.ami_id
   instance_type          = var.instance_type
-  subnet_id              = var.public_subnets
+  subnet_id              = var.subnet_id
   associate_public_ip_address= true
   vpc_security_group_ids = [var.sg_id]
   key_name               = var.key_name
